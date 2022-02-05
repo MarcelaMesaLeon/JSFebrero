cc65acc90118f3b41e1a16dbd16725ea90c22225
MarcelaMesaLeon <mardie05@hotmail.com>
2022-02-05 11:42:46 -0500

    Repaso de condicionales

---
+++ JSFebrero.js
@@ -0,0 +1,61 @@
+//1
+/*let edadUsuario= prompt("Cual es tu edad?");
+if(edadUsuario >= 18){
+    alert("Eres mayor de edad");
+} else {
+    alert("No eres mayor de edad");
+}*/
+//2
+/*const contraseña= "contrasena";
+let pregunta= prompt("Cual es tu contraseña?");
+if(pregunta===contraseña){
+    alert("Tu contraseña es correcta");
+}else{
+    alert("Tu contraseña no es correcta");
+}*/
+//3
+/*let primerNumero= prompt("Escribe un numero");
+let segundoNumero= prompt("Ahora otro");
+let resultado= primerNumero/segundoNumero;
+alert("El resultado es " + resultado);
+if(segundoNumero==0){
+    alert("Error!");
+}*/
+//4
+/*let numeroUsuario= prompt("Escribe un numero");
+if(numeroUsuario%2==0){
+    alert("El numero "+ numeroUsuario +" es par");
+}else{
+    alert("El numero "+ numeroUsuario +" es impar");
+}*/
+//5
+/*let edad= prompt("Cual es tu edad?");
+let ingresos= prompt("Cuanto es tu ingreso mensual?");
+if(edad >= 18){
+    if(ingresos >= 1000000){
+        alert("Usted debe tributar");
+    }else{
+        alert("Usted no debe tributar");
+    }
+}else{
+    alert("Usted no debe tributar");
+}*/
+//6
+/*let nombre= prompt("Cual es tu nombre?");
+let sexo= prompt("Tu sexo es masculino (m) o femenino (f)?");
+if(sexo==="f"){
+    if(nombre.charAt(0) <= "m"){
+        alert("Perteneces al grupo A");
+    }else{
+        alert("Pertences al grupo B")
+    }
+}else if (sexo==="m"){
+    if(nombre.charAt(0) >= "n"){
+        alert("Perteneces al grupo A");
+    }else{
+        alert("Pertences al grupo B");
+    }
+}*/
+
+
+
